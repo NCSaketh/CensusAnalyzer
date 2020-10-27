4ad498a4f3d8aeebe089e467fcdd5fd4fa792019
NCSaketh <nc.saketh1@gmail.com>
2020-10-27 22:39:40 +0530

    TC1.1 : Checking if given csv file returns correct records num

--- src/test/java/com/cg/censusanalyzer/StateCensusAnalyzerTest.java
+++ src/test/java/com/cg/censusanalyzer/StateCensusAnalyzerTest.java
@@ -1,4 +1,19 @@
 package com.cg.censusanalyzer;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class StateCensusAnalyzerTest {
+
+    private StateCensusAnalyzer stateCensusAnalyzer;
+
+
+    @Test
+    public void givenCSVFile_WhenRead_ShouldReturnCorrectRecordCount() {
+        stateCensusAnalyzer = new StateCensusAnalyzer();
+        int result = stateCensusAnalyzer.readCSVData();
+        Assert.assertEquals(29, result);
+    }
 }
